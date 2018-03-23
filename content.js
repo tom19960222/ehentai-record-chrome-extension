fc9416375d0796ed8d19224d8b713d2ff96e53a3
爆頭專家 <tom19960222@gmail.com>
2018-03-24 03:32:45 +0800

    Add code to track multi page viewer

--- content.js
+++ content.js
@@ -1,47 +1,95 @@
 console.log(document.URL);
-
-var parseResponse = function(resp){
-  var metadata = JSON.parse(EHapiRequest.responseText);
-  metadata = metadata.gmetadata[0];
-  return metadata;
-}
-
-var match = document.URL.match(/https:\/\/(exhentai|e-hentai).org\/g\/([0-9A-Za-z]+)\/([0-9A-Za-z]+)\/?/);
-if (match && match.length === 4){
-  console.log(`Gallery_id=${match[2]}, Gallery_token=${match[3]}`);
-
-  var EHapiRequest = new XMLHttpRequest();
-  var data = {
-    'method': 'gdata',
-    'gidlist': [
-        [match[2], match[3]]
-    ],
-    'namespace': 1
-  }
-  var apiUrl = match[1] === 'exhentai' ? 'https://exhentai.org/api.php' : 'https://api.e-hentai.org/api.php'
-
-  EHapiRequest.open('POST', apiUrl, true);
-  EHapiRequest.setRequestHeader("Content-type", "application/json");
-  EHapiRequest.onreadystatechange = function() { //Call a function when the state changes.
-    if(EHapiRequest.readyState == 4 && EHapiRequest.status == 200) {
-      var metadata = parseResponse(EHapiRequest.responseText);
-      console.log(metadata);
-
-      var serverApiRequest = new XMLHttpRequest();
-      var recordData = {
-        metadata: metadata,
-        user: 'tom19960222@gmail.com',
-        time: parseInt(new Date().valueOf() / 1000)
-      }
-      serverApiRequest.open('POST', 'https://eh-record.hsexpert.net/record', true);
-      serverApiRequest.setRequestHeader("Content-type", "application/json");
-      serverApiRequest.onreadystatechange = function() {
-        if(serverApiRequest.readyState == 4 && serverApiRequest.status == 201){
-          console.log('Send record done.');
-        }
-      }
-      serverApiRequest.send(JSON.stringify(recordData));
+class ServerRequest {
+    constructor(data) {
+        this.metadata = data.metadata;
+        this.user = data.user;
+        this.time = data.time;
     }
-  }
-  EHapiRequest.send(JSON.stringify(data));
 }
+const parseResponse = (resp) => {
+    var metadata = JSON.parse(resp);
+    metadata = metadata.gmetadata[0];
+    return metadata;
+};
+const queryEHentaiAPIServer = (apiService, galleryId, galleryToken) => {
+    return new Promise((resolve, reject) => {
+        const EHapiRequest = new XMLHttpRequest();
+        const data = {
+            'method': 'gdata',
+            'gidlist': [
+                [galleryId, galleryToken]
+            ],
+            'namespace': 1
+        };
+        const apiUrl = apiService === 'exhentai' ? 'https://exhentai.org/api.php' : 'https://api.e-hentai.org/api.php';
+        EHapiRequest.open('POST', apiUrl, true);
+        EHapiRequest.setRequestHeader("Content-type", "application/json");
+        EHapiRequest.onreadystatechange = function () {
+            if (EHapiRequest.readyState === XMLHttpRequest.DONE && EHapiRequest.status == 200) {
+                const metadata = parseResponse(EHapiRequest.responseText);
+                console.log(metadata);
+                return resolve(metadata);
+            }
+        };
+        EHapiRequest.send(JSON.stringify(data));
+    });
+};
+const sendDataToServer = (metadata) => {
+    const SERVER_URL = 'https://eh-record.hsexpert.net/record';
+    return new Promise((resolve, reject) => {
+        const serverApiRequest = new XMLHttpRequest();
+        const recordData = new ServerRequest({
+            metadata: metadata,
+            user: 'tom19960222@gmail.com',
+            time: (new Date().valueOf() / 1000)
+        });
+        serverApiRequest.open('POST', SERVER_URL, true);
+        serverApiRequest.setRequestHeader('Content-type', 'application/json');
+        serverApiRequest.onreadystatechange = function () {
+            if (serverApiRequest.readyState === XMLHttpRequest.DONE && serverApiRequest.status == 201) {
+                console.log('Send record done.');
+                return resolve();
+            }
+        };
+        serverApiRequest.send(JSON.stringify(recordData));
+    });
+};
+const main = async () => {
+    const match = document.URL.match(/https:\/\/(exhentai|e-hentai).org\/g\/([0-9A-Za-z]+)\/([0-9A-Za-z]+)\/?/);
+    const mpvPageMatch = document.URL.match(/https:\/\/(exhentai|e-hentai).org\/mpv\/([0-9A-Za-z]+)\/([0-9A-Za-z]+)\/?/);
+    if (match && match.length === 4) {
+        console.log(`Gallery_id=${match[2]}, Gallery_token=${match[3]}`);
+        const metadata = await queryEHentaiAPIServer(match[1], match[2], match[3]);
+        await sendDataToServer(metadata);
+    }
+    if (mpvPageMatch && mpvPageMatch.length === 4) {
+        mpvPageReadTracker();
+    }
+};
+const mpvPageReadTracker = () => {
+    const readPages = new Set();
+    var MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
+    var elementList = document.querySelectorAll('.mi0');
+    var observer = new MutationObserver(function (mutations) {
+        mutations.forEach(function (mutation) {
+            console.log(mutation);
+            if (mutation.type == "attributes") {
+                if (mutation.target.style.visibility === 'hidden')
+                    return;
+                const matchResult = mutation.target.id.match(/image_([0-9]+)/);
+                if (matchResult.length > 1) {
+                    if (!(readPages.has(matchResult[1]))) {
+                        readPages.add(matchResult[1]);
+                        console.log(`You are reading page ${matchResult[1]}`);
+                    }
+                }
+            }
+        });
+    });
+    for (const element of elementList) {
+        observer.observe(element, {
+            attributes: true //configure it to listen to attribute changes
+        });
+    }
+};
+main().then(() => { });
