cdab62a4650706287d55c1b5c3a6b4ff5e2e0138
爆頭專家 <tom19960222@gmail.com>
2018-06-09 20:54:34 +0800

    Update test

--- tests/ServerRequestAction.test.ts
+++ tests/ServerRequestAction.test.ts
@@ -18,7 +18,7 @@ test('Get metadata from server', async () => {
   const response = `{"gmetadata":[{"gid":1217029,"token":"ba12731380","archiver_key":"423593--b00a566a8b16ab04b1fdab24ef600ff5510a5bb8","title":"[Anthology] Kono Haru, Kanojo to Issho ni Sotsugyou Shimashita ~Sensei x JK Kindan H Hen~ [Digital]","title_jpn":"[\u30a2\u30f3\u30bd\u30ed\u30b8\u30fc] \u3053\u306e\u6625\u3001\u5f7c\u5973\u3068\u4e00\u7dd2\u306b\u5352\u696d\u3057\u307e\u3057\u305f \uff5e\u5148\u751f\u00d7JK\u7981\u65adH\u7de8\uff5e [DL\u7248]","category":"Manga","thumb":"https:\/\/exhentai.org\/t\/5b\/d9\/5bd9bc5a1ec890d76f984f55ce714cabec20efef-236092-1057-1500-jpg_l.jpg","uploader":"DarkMGA","posted":"1524906592","filecount":"132","filesize":59991247,"expunged":false,"rating":"4.43","torrentcount":"2","tags":["artist:harukichi","artist:ichiko","artist:izumiya otoha","artist:samidare setsuna","artist:tachibana yuu","male:glasses","male:schoolboy uniform","female:big breasts","female:blowjob","female:paizuri","female:pantyhose","female:schoolgirl uniform","female:twintails","anthology"]}]}`;
 
   mockAxios.onPost().reply(200, response, responseHeaders);
-  const result = await serverRequestActionInstance.queryMetadataFromEHentaiAPIServer('exhentai', '1217029', 'ba12731380');
+  const result = await serverRequestActionInstance.queryMetadataFromEHentaiAPIServer('exhentai', 1217029, 'ba12731380');
 
   expect(result).toBeDefined();
   expect(result.gid).toBe(1217029);
--- tests/content.test.ts
+++ tests/content.test.ts
@@ -2,24 +2,30 @@ import { ServerRequestAction } from '../src/ServerRequestAction';
 import { mpvPageReadTracker } from '../src/mpvPageReadTracker';
 import { Main } from '../src/content';
 
+let mockQueryMetadataFromEHentaiAPIServer = jest.fn();
+let mockSendMetadataToServer = jest.fn();
 jest.mock('../src/ServerRequestAction', () => {
   return {
     ServerRequestAction: jest.fn().mockImplementation(() => {
       return {
-        queryMetadataFromEHentaiAPIServer: jest.fn(),
-        sendMetadataToServer: jest.fn()
+        queryMetadataFromEHentaiAPIServer: mockQueryMetadataFromEHentaiAPIServer,
+        sendMetadataToServer: mockSendMetadataToServer,
+        sendEventToServer: jest.fn()
       }
     })
   }
 });
+// jest.mock('../src/ServerRequestAction');
 jest.mock('../src/mpvPageReadTracker');
 
 beforeEach(() => {
-  ServerRequestAction.mockClear();
+  mockQueryMetadataFromEHentaiAPIServer.mockClear();
+  mockSendMetadataToServer.mockClear();
+  // ServerRequestAction.mockClear();
   mpvPageReadTracker.mockClear();
 });
 
-test('main function should no nothing in other sites', async () => {
+test('main function should do nothing in other sites', async () => {
   const mockDocument = new Document();
   Object.defineProperty(mockDocument, 'URL', {
     configurable: true,
@@ -28,8 +34,9 @@ test('main function should no nothing in other sites', async () => {
 
   await new Main(mockDocument).start();
 
-  expect(ServerRequestAction).not.toBeCalled();
   expect(mpvPageReadTracker).not.toBeCalled();
+  expect(mockQueryMetadataFromEHentaiAPIServer).not.toBeCalled();
+  expect(mockSendMetadataToServer).not.toBeCalled();
 });
 
 test('main function should query and send metadata in book detail page', async () => {
@@ -51,9 +58,10 @@ test('Can extract information in gallery URL', () => {
   const info = main.extractURLInformation('https://exhentai.org/g/1236090/1e57b4def9/');
   expect(info).toEqual({
     position: 'gallery',
-    galleryId: '1236090',
+    galleryId: 1236090,
     galleryToken: '1e57b4def9',
-    site: 'exhentai'
+    site: 'exhentai',
+    page: null
   });
 });
 
@@ -63,9 +71,10 @@ test('Can extract information in multi page viewer URL', () => {
   const info = main.extractURLInformation('https://exhentai.org/mpv/1236090/1e57b4def9');
   expect(info).toEqual({
     position: 'mpv',
-    galleryId: '1236090',
+    galleryId: 1236090,
     galleryToken: '1e57b4def9',
-    site: 'exhentai'
+    site: 'exhentai',
+    page: null
   });
 });
 
@@ -75,7 +84,7 @@ test('Can extract information in single page viewer URL', () => {
   const info = main.extractURLInformation('https://e-hentai.org/s/e73e7138f3/1236080-3');
   expect(info).toEqual({
     position: 'single',
-    galleryId: '1236080',
+    galleryId: 1236080,
     galleryToken: 'e73e7138f3',
     site: 'e-hentai',
     page: 3
