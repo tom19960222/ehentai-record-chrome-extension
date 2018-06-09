f56b3618fbeae625e8ce059e7a3141ce2f67d7b1
爆頭專家 <tom19960222@gmail.com>
2018-06-09 18:12:55 +0800

    Refactor with axios

--- src/ServerRequestAction.ts
+++ src/ServerRequestAction.ts
@@ -1,4 +1,6 @@
-class Metadata {
+import axios from 'axios';
+
+export class Metadata {
   archiver_key: string;
   category: string;
   expunged: boolean;
@@ -20,7 +22,7 @@ class Metadata {
   }
 }
 
-class MetadataServerRequest {
+export class MetadataServerRequest {
   metadata: Metadata;
   user: string;
   time: number;
@@ -28,22 +30,39 @@ class MetadataServerRequest {
   constructor (data: any) {
     this.metadata = data.metadata;
     this.user = data.user;
-    this.time = data.time;
+    this.time = Math.trunc(new Date().valueOf() / 1000);
   }
 }
 
-enum PageTypeEnum {
+export enum ViewerTypeEnum {
   BookList = 'book_list',
   BookDetail = 'book_detail',
   BookSingleImagePage = 'book_single_image_page',
   MultiPageViewer = 'multi_page_viewer'
 }
 
-class EventServerRequest {
-  page_type: PageTypeEnum;
+export class ReadEvent {
+  viewer_type: ViewerTypeEnum;
   gid: number;
   page_number: number | null;
   time: number;
+
+  constructor({viewer_type, gid, page_number}: {viewer_type: ViewerTypeEnum, gid: number, page_number: number}){
+    this.viewer_type = viewer_type;
+    this.gid = gid;
+    this.page_number = page_number;
+    this.time = Math.trunc(new Date().valueOf() / 1000)
+  }
+}
+
+export class EventServerRequest {
+  event: ReadEvent;
+  user: string;
+
+  constructor({event, user}: {event: ReadEvent, user: string}){
+    this.event = event;
+    this.user = user;
+  }
 }
 
 export class ServerRequestAction {
@@ -53,50 +72,40 @@ export class ServerRequestAction {
     return metadata;
   }
 
-  queryMetadataFromEHentaiAPIServer = (apiService: string, galleryId: string, galleryToken: string) : Promise<Metadata> => {
-    return new Promise((resolve, reject) => {
-      const self = this;
-      const EHapiRequest = new XMLHttpRequest();
-      const data = {
-        'method': 'gdata',
-        'gidlist': [
-            [galleryId, galleryToken]
-        ],
-        'namespace': 1
-      }
-      const apiUrl = apiService === 'exhentai' ? 'https://exhentai.org/api.php' : 'https://api.e-hentai.org/api.php'
-    
-      EHapiRequest.open('POST', apiUrl, true);
-      EHapiRequest.setRequestHeader("Content-type", "application/json"); 
-      EHapiRequest.onreadystatechange = function() { // Call a function when the state changes.
-        if(EHapiRequest.readyState === 4 && EHapiRequest.status === 200) {
-          const metadata = self.parseResponse(EHapiRequest.responseText);
-          return resolve(metadata);
-        }
-      }
-      EHapiRequest.send(JSON.stringify(data));
-    })
+  queryMetadataFromEHentaiAPIServer = async (apiService: string, galleryId: string, galleryToken: string) : Promise<Metadata> => {
+    const data = {
+      'method': 'gdata',
+      'gidlist': [
+          [galleryId, galleryToken]
+      ],
+      'namespace': 1
+    }
+    const apiUrl = apiService === 'exhentai' ? 'https://exhentai.org/api.php' : 'https://api.e-hentai.org/api.php';
+    const headers = {'Content-Type': 'application/json'};
+    const response = await axios.post(apiUrl, data, { headers });
+    console.log('response', response);
+    return new Metadata(response.data.gmetadata[0]);
   }
 
-  sendMetadataToServer = (metadata: object) : Promise<undefined> => {
+  sendMetadataToServer = async (metadata: Metadata) : Promise<MetadataServerRequest> => {
     const SERVER_URL = 'https://eh-record.hsexpert.net/record';
-  
-    return new Promise((resolve, reject) => {
-      const serverApiRequest = new XMLHttpRequest();
-      const recordData = new MetadataServerRequest({
-        metadata: metadata,
-        user: 'tom19960222@gmail.com',
-        time: (new Date().valueOf() / 1000)
-      });
-      serverApiRequest.open('POST', SERVER_URL, true);
-      serverApiRequest.setRequestHeader('Content-type', 'application/json');
-      serverApiRequest.onreadystatechange = function() {
-        if(serverApiRequest.readyState === XMLHttpRequest.DONE && serverApiRequest.status == 201){
-          console.log('Send record done.');
-          return resolve();
-        }
-      }
-      serverApiRequest.send(JSON.stringify(recordData));
-    })
+    const data = new MetadataServerRequest({
+      metadata: metadata,
+      user: 'tom19960222@gmail.com',
+    });
+    const headers = {'Content-Type': 'application/json'};
+    const response = await axios.post(SERVER_URL, data, { headers });
+    return new MetadataServerRequest(response.data);
+  }
+
+  sendEventToServer = async (event: ReadEvent): Promise<EventServerRequest> => {
+    const SERVER_URL = 'https://eh-record.hsexpert.net/event';
+    const data = new EventServerRequest({
+      event,
+      user: 'tom19960222@gmail.com'
+    });
+    const headers = {'Content-Type': 'application/json'};
+    const response = await axios.post(SERVER_URL, data, { headers });
+    return new EventServerRequest(response.data);
   }
 }
--- tests/ServerRequestAction.test.ts
+++ tests/ServerRequestAction.test.ts
@@ -1,53 +1,23 @@
-import { ServerRequestAction } from '../src/ServerRequestAction';
-import { newMockXhr } from 'mock-xmlhttprequest';
+import { ServerRequestAction, Metadata, MetadataServerRequest, ReadEvent, EventServerRequest, ViewerTypeEnum } from '../src/ServerRequestAction';
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
 
+const mockAxios = new MockAdapter(axios);
 let serverRequestActionInstance : ServerRequestAction;
-let MockXMLHttpRequest: any;
 
-const createXHRmock = () => {
-  const mockObj = {
-    open: jest.fn(),
-    send: jest.fn(),
-    setRequestHeader: jest.fn(),
-    mockRequestFinished: function() {
-      if(!this.onreadystatechange) throw new Error('No onreadystatechange function!');
-      this.readyState = 4; // XMLHttpRequest.DONE
-      this.status = 200;
-      this.responseText = '';
-      this.onreadystatechange();
-    }
-  };
-
-  const xhrMockClass = function () {
-    const self = this;
-      return {
-        open: mockObj.open,
-        send: mockObj.send,
-        setRequestHeader: mockObj.setRequestHeader,
-        mockRequestFinished: mockObj.mockRequestFinished.bind(self)
-      };
-  };
-
-  window.XMLHttpRequest = jest.fn(xhrMockClass);
-  return mockObj;
+const responseHeaders = {
+  'Content-Type': 'application/json',
 }
 
 beforeEach(() => {
   serverRequestActionInstance = new ServerRequestAction();
-  MockXMLHttpRequest = newMockXhr();
-  global.XMLHttpRequest = MockXMLHttpRequest;
+  mockAxios.reset();
 });
 
 test('Get metadata from server', async () => {
-  MockXMLHttpRequest.onSend = function(xhr) {
-    const response = `{"gmetadata":[{"gid":1217029,"token":"ba12731380","archiver_key":"423593--b00a566a8b16ab04b1fdab24ef600ff5510a5bb8","title":"[Anthology] Kono Haru, Kanojo to Issho ni Sotsugyou Shimashita ~Sensei x JK Kindan H Hen~ [Digital]","title_jpn":"[\u30a2\u30f3\u30bd\u30ed\u30b8\u30fc] \u3053\u306e\u6625\u3001\u5f7c\u5973\u3068\u4e00\u7dd2\u306b\u5352\u696d\u3057\u307e\u3057\u305f \uff5e\u5148\u751f\u00d7JK\u7981\u65adH\u7de8\uff5e [DL\u7248]","category":"Manga","thumb":"https:\/\/exhentai.org\/t\/5b\/d9\/5bd9bc5a1ec890d76f984f55ce714cabec20efef-236092-1057-1500-jpg_l.jpg","uploader":"DarkMGA","posted":"1524906592","filecount":"132","filesize":59991247,"expunged":false,"rating":"4.43","torrentcount":"2","tags":["artist:harukichi","artist:ichiko","artist:izumiya otoha","artist:samidare setsuna","artist:tachibana yuu","male:glasses","male:schoolboy uniform","female:big breasts","female:blowjob","female:paizuri","female:pantyhose","female:schoolgirl uniform","female:twintails","anthology"]}]}`;
-    const responseHeaders = {
-      'Content-Type': 'application/json',
-    }
-    console.log('ready to respond!');
-    xhr.respond(200, responseHeaders, response);
-  };
-  
+  const response = `{"gmetadata":[{"gid":1217029,"token":"ba12731380","archiver_key":"423593--b00a566a8b16ab04b1fdab24ef600ff5510a5bb8","title":"[Anthology] Kono Haru, Kanojo to Issho ni Sotsugyou Shimashita ~Sensei x JK Kindan H Hen~ [Digital]","title_jpn":"[\u30a2\u30f3\u30bd\u30ed\u30b8\u30fc] \u3053\u306e\u6625\u3001\u5f7c\u5973\u3068\u4e00\u7dd2\u306b\u5352\u696d\u3057\u307e\u3057\u305f \uff5e\u5148\u751f\u00d7JK\u7981\u65adH\u7de8\uff5e [DL\u7248]","category":"Manga","thumb":"https:\/\/exhentai.org\/t\/5b\/d9\/5bd9bc5a1ec890d76f984f55ce714cabec20efef-236092-1057-1500-jpg_l.jpg","uploader":"DarkMGA","posted":"1524906592","filecount":"132","filesize":59991247,"expunged":false,"rating":"4.43","torrentcount":"2","tags":["artist:harukichi","artist:ichiko","artist:izumiya otoha","artist:samidare setsuna","artist:tachibana yuu","male:glasses","male:schoolboy uniform","female:big breasts","female:blowjob","female:paizuri","female:pantyhose","female:schoolgirl uniform","female:twintails","anthology"]}]}`;
+
+  mockAxios.onPost().reply(200, response, responseHeaders);
   const result = await serverRequestActionInstance.queryMetadataFromEHentaiAPIServer('exhentai', '1217029', 'ba12731380');
 
   expect(result).toBeDefined();
@@ -57,7 +27,7 @@ test('Get metadata from server', async () => {
 })
 
 test('Send metadata to server', async () => {
-  const metadata = {
+  const metadata = new Metadata({
     "gid": 1217029,
     "token": "ba12731380",
     "archiver_key": "423593--b00a566a8b16ab04b1fdab24ef600ff5510a5bb8",
@@ -88,6 +58,30 @@ test('Send metadata to server', async () => {
       "female:twintails",
       "anthology"
     ]
-  }
+  });
+  const metadataRequest = new MetadataServerRequest({
+    metadata,
+    user: 'test@test.com'
+  });
+  mockAxios.onPost().reply(201, metadataRequest, responseHeaders);
   
+  const result = await serverRequestActionInstance.sendMetadataToServer(metadata);
+  expect(result.metadata).toEqual(metadata);
+  expect(result.user).toEqual('test@test.com');
+})
+
+test('Send event to server', async () => {
+  const event = new ReadEvent({
+    viewer_type: ViewerTypeEnum.BookSingleImagePage,
+    gid: 1217029,
+    page_number: 1
+  });
+  const eventRequest = new EventServerRequest({
+    event, user: 'test@test.com'
+  })
+  mockAxios.onPost().reply(201, eventRequest, responseHeaders);
+
+  const result = await serverRequestActionInstance.sendEventToServer(event);
+  expect(result.event).toEqual(event);
+  expect(result.user).toEqual('test@test.com');
 })
